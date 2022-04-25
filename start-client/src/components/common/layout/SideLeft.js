815351de8a0fd791368fd148ee05f9225dabb92d
Chris Cheetham <chris@cheetham.com>
2022-04-25 12:08:33 -0400

    Update InitializrService text

--- start-client/src/components/common/layout/SideLeft.js
+++ start-client/src/components/common/layout/SideLeft.js
@@ -209,9 +209,9 @@ function SideLeft() {
                       <a
                         target='_blank'
                         rel='noopener noreferrer'
-                        href='https://github.com/SteeltoeOSS/InitializrApi'
+                        href='https://github.com/SteeltoeOSS/InitializrService'
                       >
-                        InitializrAPI
+                        InitializrService
                       </a>{' '}
                       <text id='apiVersion' />
                     </span>
